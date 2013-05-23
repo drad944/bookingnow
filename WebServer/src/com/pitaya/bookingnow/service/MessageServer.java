f26c911db586a0e11e3b61c892aa93699d141aa6
zhangrunmeng@gmail.com <zhangrunmeng@gmail.com@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-05-23 15:27:15 +0000

    Add test messageserver code

--- WebServer/src/com/pitaya/bookingnow/service/MessageServer.java
+++ WebServer/src/com/pitaya/bookingnow/service/MessageServer.java
@@ -6,15 +6,21 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 class ClientAgent extends Thread{
 	
+	private static Log logger =  LogFactory.getLog(ClientAgent.class);
 	Socket client_socket;
 	MessageServer server;
 	PrintWriter out;
-	String userid;
+	BufferedReader in;
+	String username;
 	String role;
 	
 	ClientAgent(MessageServer server, Socket socket){
@@ -24,30 +30,75 @@ class ClientAgent extends Thread{
 	
 	public void run(){
 		try{
-			BufferedReader in = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));   
+			in = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));   
 			out = new PrintWriter(client_socket.getOutputStream());   
 			String message = null;
 			while((message = in.readLine()) != null){
-				System.out.println(message);
+				if(message.startsWith("login:")){
+					String[] logininfos = message.substring("login:".length()).split(":");
+					if(logininfos.length == 3 && logininfos[2].equals("123456")){
+						this.username = logininfos[0];
+						this.role = logininfos[1];
+						if(this.server.addClient(this)){
+							this.sendMessage("Login successfully!");
+							logger.info("Success to connect to "+ this.username);
+						} else {
+							this.sendMessage("Can't login two clients with same user!");
+						}
+					} else {
+						this.sendMessage("Fail to login!");
+					}
+				} else {
+					if(this.username == null){
+						this.sendMessage("Login first!");
+					} else {
+						logger.info("["+this.username+"]" + message);
+					}
+				}
 			}
 		 }catch (Exception e){
 	        e.printStackTrace();
-	        if(client_socket != null){
-				try {
-					client_socket.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-	        }
+		 }finally{
+	        try {
+				 in.close();
+				 out.close();
+				 if(client_socket != null){
+					 client_socket.close();
+				 }
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 		 }
 	}
 	
-	void sendMessage(String message){
-		out.write(message);
+	boolean sendMessage(String message){
+		try {
+			out.println(message);
+			out.flush();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	void shutdown(){
+		 try {
+			 this.sendMessage("bye");
+			 in.close();
+			 out.close();
+			 if(client_socket != null){
+				 client_socket.close();
+			 }
+			 logger.info("Success to shutdown connect to " + this.username);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
 class MessageServerThread extends Thread{
+	private static Log logger =  LogFactory.getLog(MessageServerThread.class);
     boolean flag = true;
     MessageServer server;
       
@@ -63,9 +114,10 @@ class MessageServerThread extends Thread{
                 ClientAgent clientAgent = new ClientAgent(this.server, client_socket);  
                 clientAgent.start( );
         	}
+        	logger.info("Success to shutdown message server thread");
         } catch( Exception e ) {  
-            e.printStackTrace( );  
-        } finally {
+            e.printStackTrace( );
+        } finally{
         	if(client_socket != null){
         		try {
 					client_socket.close();
@@ -84,6 +136,7 @@ class MessageServerThread extends Thread{
 public class MessageServer {
 	
 	private static MessageServer _instance;
+	private static Log logger =  LogFactory.getLog(MessageServer.class);
 	
 	ServerSocket serverSocket;
 	
@@ -94,7 +147,7 @@ public class MessageServer {
 	
 	private MessageServer(){
 		this.hasStarted = false;
-		groups = new HashMap<String, Map<String, ClientAgent>>();
+		groups = new ConcurrentHashMap <String, Map<String, ClientAgent>>();
 	}
 	
 	public static MessageServer getInstance(){
@@ -111,18 +164,19 @@ public class MessageServer {
 	            serverSocket = new ServerSocket(this.port);
 	            serverThread = new MessageServerThread(this);  
 	            serverThread.start();
+	            logger.info("Success to start message server thread");
 	            this.hasStarted = true;
 	            return true;
 	        } catch(IOException e) {
-	            return false;
-	        } finally {
+	        	e.printStackTrace();
 	        	if(serverSocket != null){
 	        		try {
 						serverSocket.close();
-					} catch (IOException e) {
-						e.printStackTrace();
+					} catch (IOException ex) {
+						ex.printStackTrace();
 					}
 	        	}
+	            return false;
 	        }
 		}
 		return true;
@@ -134,6 +188,12 @@ public class MessageServer {
         	if(serverSocket != null){
         		try {
 					serverSocket.close();
+					this.hasStarted = false;
+					for(Entry<String, Map<String, ClientAgent>> entry : groups.entrySet()){
+						for(Entry<String, ClientAgent> subentry : ((Map<String, ClientAgent>)entry.getValue()).entrySet()){
+							subentry.getValue().shutdown();
+						}
+					}
 					return true;
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -144,8 +204,32 @@ public class MessageServer {
 		return true;
 	}
 	
-	void addClient(ClientAgent clientAgent){
-		
+	public boolean sendMessageToGroup(String groupType, String message){
+		Map<String, ClientAgent> group = this.groups.get(groupType);
+		if(group != null){
+			boolean success = true;
+			for(Entry<String, ClientAgent> entry : group.entrySet()){
+				if(!entry.getValue().sendMessage(message)){
+					logger.error("Fail to send message to" + entry.getKey() + ":" + message);
+					success = false;
+				}
+			}
+			return success;
+		}
+		return false;
+	}
+	
+	boolean addClient(ClientAgent clientAgent){
+		Map<String, ClientAgent> group = this.groups.get(clientAgent.role);
+		if(group == null){
+			group = new ConcurrentHashMap<String, ClientAgent>();
+		}
+		if(group.get(clientAgent.username) == null){
+			group.put(clientAgent.username, clientAgent);
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 }
