a5d8dabf6d46b077dc2b2ec2aaa6e7adcd053471
zhangrunmeng@gmail.com <zhangrunmeng@gmail.com@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-05-23 15:27:15 +0000

    Add test messageserver code

---
+++ trunk/WebServer/src/com/pitaya/bookingnow/service/Constants.java
@@ -0,0 +1,10 @@
+package com.pitaya.bookingnow.service;
+
+public class Constants {
+
+	public static String KITCHEN_GP = "kitchen";
+	public static String WAITER_GP = "waiter";
+	public static String WELCOME_GP = "welcomer";
+	public static String CASH_GP = "cash";
+	
+}
---
+++ trunk/WebServer/test/com/pitaya/bookingnow/service/MessageServerTest.java
@@ -0,0 +1,41 @@
+package com.pitaya.bookingnow.service;
+
+import junit.framework.TestCase;
+
+public class MessageServerTest extends TestCase {
+
+		MessageServer messageServer;
+	
+		@Override 
+		public void setUp(){
+			messageServer = MessageServer.getInstance();
+			messageServer.start(19191);
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			for(int i=0; i < 1; i++){
+				new Client().start();
+			}
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	
+		
+		public void testSendToGroup(){
+//			messageServer.sendMessageToGroup(Constants.CASH_GP, "有新的订单需要结帐");
+//			messageServer.sendMessageToGroup(Constants.KITCHEN_GP, "新的菜品加工条目");
+			System.out.println("In test case");
+			messageServer.sendMessageToGroup(Constants.WAITER_GP, "菜品加工好了");
+		}
+		
+		@Override 
+		public void tearDown(){
+			messageServer.shutdown();
+		}
+		
+}
