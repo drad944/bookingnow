27c082e7294efd901fb796c8b7bf83b0ff7de688
zhangrunmeng@gmail.com <zhangrunmeng@gmail.com@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-05-26 05:08:16 +0000

    Enhance message service to support transfer object via json

--- trunk/WebServer/src/com/pitaya/bookingnow/service/Constants.java
+++ trunk/WebServer/src/com/pitaya/bookingnow/service/Constants.java
@@ -2,9 +2,9 @@ package com.pitaya.bookingnow.service;
 
 public class Constants {
 
-	public static String KITCHEN_GP = "kitchen";
-	public static String WAITER_GP = "waiter";
-	public static String WELCOME_GP = "welcomer";
-	public static String CASH_GP = "cash";
+	public static String KITCHEN_ROLE = "kitchen";
+	public static String WAITER_ROLE = "waiter";
+	public static String WELCOME_ROLE = "welcomer";
+	public static String CASH_ROLE = "cash";
 	
 }
