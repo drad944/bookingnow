801a8b1681df27d2682720d37e1dcf8374d7537a
hzeng135 <hzeng135@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-05-27 16:16:20 +0000

    fix many exception while start and deploy on tomcat 7.0

--- WebServer/configuration/mysql.sql
+++ WebServer/configuration/mysql.sql
@@ -11,7 +11,7 @@ flush privileges;
 
 
 
-create table pitaya_user(uid bigint not null auto_increment primary key,name varchar(20),passwd varchar(20),role varchar(10),sex varchar(2),birthday date);
+create table pitaya_user(aid integer not null auto_increment primary key,name varchar(20),passwd varchar(20),role varchar(10),sex varchar(2),birthday date);
 
 insert into pitaya_user values (1,'zhang','123456','boss','m','1971/5/8');
 
