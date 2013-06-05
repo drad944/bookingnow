592578efa63a87b1e70626eefe760839d7fc771b
zhangrunmeng@gmail.com <zhangrunmeng@gmail.com@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-06-05 12:55:17 +0000

    Add message service back

--- trunk/WebServer/src/com/pitaya/bookingnow/message/Message.java
+++ trunk/WebServer/src/com/pitaya/bookingnow/message/Message.java
@@ -10,12 +10,13 @@ public class Message implements Serializable{
 	private static final long serialVersionUID = 6356895567978585140L;
 	
 	private String key;
-	protected String type; 
+	private String type; 
 	
 	public Message(){}
 	
 	public Message(String key){
 		this.key = key;
+		this.type = this.getClass().getName();
 	}
 	
 	public void setKey(String key){
