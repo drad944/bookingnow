d336e371a011d0f99db9b6c7b341ad7faad800c4
hzeng135 <hzeng135@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-08-17 15:53:29 +0000

    add cutImage function

--- WebSecurityServer/src/com/pitaya/bookingnow/util/ImageUtil.java
+++ WebSecurityServer/src/com/pitaya/bookingnow/util/ImageUtil.java
@@ -3,15 +3,17 @@ package com.pitaya.bookingnow.util;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.awt.image.CropImageFilter;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageFilter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReadParam;
@@ -103,6 +105,42 @@ public class ImageUtil {
 			BufferedImage src = ImageIO.read(f);
 			ImageIO.write(src, imageType, new File(targetPath));
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void cutImage(String imagePath, String imageType, int x, int y,
+			int w, int h) {
+		try {
+			Image img;
+			ImageFilter cropFilter;
+			// 读取源图像
+			BufferedImage bi = ImageIO.read(new File(imagePath));
+			int srcWidth = bi.getWidth(); // 源图宽度
+			int srcHeight = bi.getHeight(); // 源图高度
+
+			// 若原图大小大于切片大小，则进行切割
+			if (srcWidth >= w && srcHeight >= h) {
+				Image image = bi.getScaledInstance(srcWidth, srcHeight,
+						Image.SCALE_DEFAULT);
+
+				int x1 = x * srcWidth / 400;
+				int y1 = y * srcHeight / 270;
+				int w1 = w * srcWidth / 400;
+				int h1 = h * srcHeight / 270;
+
+				cropFilter = new CropImageFilter(x1, y1, w1, h1);
+				img = Toolkit.getDefaultToolkit().createImage(
+						new FilteredImageSource(image.getSource(), cropFilter));
+				BufferedImage tag = new BufferedImage(w1, h1,
+						BufferedImage.TYPE_INT_RGB);
+				Graphics g = tag.getGraphics();
+				g.drawImage(img, 0, 0, null); // 绘制缩小后的图
+				g.dispose();
+				// 输出为文件
+				ImageIO.write(tag, imageType, new File(imagePath));
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
