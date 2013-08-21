8a69b7f6a14a7fb47de878d1e5082f0c4ca0701b
zhangrunmeng@gmail.com <zhangrunmeng@gmail.com@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-08-21 13:48:27 +0000

    Checked in the file forgotten

---
+++ WebSecurityServer/src/com/pitaya/bookingnow/service/impl/ConnectionCheckTask.java
@@ -0,0 +1,20 @@
+package com.pitaya.bookingnow.service.impl;
+
+import java.util.TimerTask;
+
+public class ConnectionCheckTask extends TimerTask {
+
+	private MessageService service;
+	
+	public ConnectionCheckTask(MessageService service){
+		this.service = service;
+	}
+	
+	@Override
+	public void run() {
+		for(ClientAgent client : service.getClients()){
+			client.sendHeartBeat();
+		}
+	}
+
+}
