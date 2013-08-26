2b4a4ad3e0d9deb7491704985132bdecb3279d33
zhangrunmeng@gmail.com <zhangrunmeng@gmail.com@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-08-26 17:20:00 +0000

    Add preference feature

---
+++ trunk/Android/BookingNow/src/com/pitaya/bookingnow/app/views/SettingsFragment.java
@@ -0,0 +1,57 @@
+package com.pitaya.bookingnow.app.views;
+
+import com.pitaya.bookingnow.app.HomeActivity;
+import com.pitaya.bookingnow.app.R;
+import com.pitaya.bookingnow.app.data.MessageHandler;
+import com.pitaya.bookingnow.app.preference.*;
+import com.pitaya.bookingnow.app.service.MessageService;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends PreferenceFragment {
+
+	private MessageHandler mMessageHandler;
+	private MessageService mMessageService;
+	private boolean mIsBound = false;
+	private ServiceConnection mConnection;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preference);
+        ButtonPreference upbtn = (ButtonPreference)this.findPreference(SettingsContentView.CHECK_MENU_UPDATE);
+        upbtn.setValues(new String[]{"获取最新菜单", "更新"});
+        upbtn.setClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(getActivity() instanceof HomeActivity){
+					((HomeActivity)getActivity()).checkMenuUpdate();
+				}
+			}
+			
+		});
+        ButtonPreference cleanbtn = (ButtonPreference)this.findPreference(SettingsContentView.CLEAN_DATA);
+        cleanbtn.setValues(new String[]{"清除菜单，订单数据", "清除"});
+        ButtonPreference connectbtn = (ButtonPreference)this.findPreference(SettingsContentView.CONNECT_SERVER);
+        connectbtn.setValues(new String[]{"连接服务器", "连接"});
+        connectbtn.setClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				getActivity().startService(new Intent(getActivity(), MessageService.class));
+			}
+			
+		});
+    }
+
+}
