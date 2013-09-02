8fa006fb0953f431dbe3937de447e0d825226f89
hzeng135 <hzeng135@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-09-02 16:59:58 +0000

    add userlogin funciton in UI side.

---
+++ trunk/WebSecurityServer/WebContent/js/pitaya/userLogin.js
@@ -0,0 +1,71 @@
+function initUserLoginLocaleElements() {
+	var option = {
+			fallbackLng: 'zh',
+			lng: 'en-US',
+	//		lng: 'zh-CN',
+			resGetPath: 'resources/locales/__lng__/__ns__.json',
+			getAsync: false,
+			ns: 'bookingnow.top.menu'
+		};
+	 
+	i18n.init(option);
+	$("#userLoginDiv").i18n();
+}
+
+function initUserLoginElements() {
+	
+    $("#userLoginDiv").jqxExpander({ toggleMode: 'none', width: '300px', showArrow: false, theme: theme });
+    $('#user_login_button').jqxButton({ width: 60, height: 25, theme: theme });
+    $('#user_register_button').jqxButton({ width: 60, height: 25, theme: theme });
+    
+	$('#user_account').jqxInput({width: 120, height: 20, theme: theme });
+	$('#user_password').jqxInput({width: 120, height: 20, theme: theme });
+    
+    $('#userLoginForm').jqxValidator({
+     rules: [
+            { input: '#user_account', message: i18n.t("userLogin.validation.message.requireAccount"), action: 'keyup, blur', rule: 'required' },
+            { input: '#user_account', message: i18n.t("userLogin.validation.message.accountLength"), action: 'keyup, blur', rule: 'length=3,15' },
+            { input: '#user_password', message: i18n.t("userLogin.validation.message.requirePassword"), action: 'keyup, blur', rule: 'required' },
+            { input: '#user_password', message: i18n.t("userLogin.validation.message.passwordLength"), action: 'keyup, blur', rule: 'length=6,20' }
+            ], 
+            theme: theme
+    });
+	
+};
+
+	
+function addUserLoginEventListeners() {
+	$("#user_register_button").on('click', function() {
+		$('#userLoginForm').jqxValidator('hide');
+	});
+
+	$("#user_login_button").on('click', function() {
+		$('#userLoginForm').jqxValidator('validate');
+	});
+
+	$('#userLoginForm').on('validationSuccess', function(event) {
+		$('#user_login_button').submit();
+		
+		/*
+		var userAccount = $('#user_account').val();
+		var userPassword = $('#user_password').val();
+		if (userAccount != null && userPassword != null) {
+
+			$.post("loginUser.action", {
+				"user.account" : userAccount,
+				"user.password" : userPassword
+			},function(user) {
+				if (user != null && user["id"] != null) {
+
+				} else {
+					$("#eventLog").text("注册失败");
+				}
+			});
+			
+		}
+		*/
+	});
+
+}
+	
+	
