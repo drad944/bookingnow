8cb8ad8ab4f42970dc21caa8867298913786a467
hzeng135 <hzeng135@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-09-03 14:51:11 +0000

    fixed locale issue in login page. fixed can not get callback info from server when login.

--- trunk/WebSecurityServer/WebContent/js/pitaya/userLogin.js
+++ trunk/WebSecurityServer/WebContent/js/pitaya/userLogin.js
@@ -1,68 +1,90 @@
-function initUserLoginLocaleElements() {
-	var option = {
-			fallbackLng: 'zh',
-			lng: 'en-US',
-	//		lng: 'zh-CN',
-			resGetPath: 'resources/locales/__lng__/__ns__.json',
-			getAsync: false,
-			ns: 'bookingnow.top.menu'
-		};
-	 
-	i18n.init(option);
-	$("#userLoginDiv").i18n();
-}
+var userLogin = {
+		
+		leave : function (){
+			$("#user_register_button").unbind('click');
+			$("#user_login_button").unbind('click');
+			$("#userLoginForm").unbind('validationSuccess');
+		},
 
-function initUserLoginElements() {
-	
-    $("#userLoginDiv").jqxExpander({ toggleMode: 'none', width: '300px', showArrow: false, theme: theme });
-    $('#user_login_button').jqxButton({ width: 60, height: 25, theme: theme });
-    $('#user_register_button').jqxButton({ width: 60, height: 25, theme: theme });
-    
-	$('#user_account').jqxInput({width: 120, height: 20, theme: theme });
-	$('#user_password').jqxInput({width: 120, height: 20, theme: theme });
-    
-    $('#userLoginForm').jqxValidator({
-     rules: [
-            { input: '#user_account', message: i18n.t("userLogin.validation.message.requireAccount"), action: 'keyup, blur', rule: 'required' },
-            { input: '#user_account', message: i18n.t("userLogin.validation.message.accountLength"), action: 'keyup, blur', rule: 'length=3,15' },
-            { input: '#user_password', message: i18n.t("userLogin.validation.message.requirePassword"), action: 'keyup, blur', rule: 'required' },
-            { input: '#user_password', message: i18n.t("userLogin.validation.message.passwordLength"), action: 'keyup, blur', rule: 'length=6,20' }
-            ], 
-            theme: theme
-    });
-	
-};
+		visit : function (){
+			this.init();
+		},
+		init : function(){
+			var me = this;
+			me.initUserLoginLocaleElements();
+			me.initUserLoginElements();
+			me.addUserLoginEventListeners();
+		},
+		initUserLoginLocaleElements:function () {
+			var option = {
+					fallbackLng: 'zh',
+					lng: 'zh',
+			//		lng: 'zh-CN',
+					resGetPath: 'resources/locales/__lng__/__ns__.json',
+					getAsync: false,
+					ns: 'bookingnow.top.menu'
+				};
+			 
+			i18n.init(option);
+			$("#userLoginDiv").i18n();
+			$("#user_login_button").val(i18n.t("userLogin.loginButtonLabel"));
+			$("#user_register_button").val(i18n.t("userLogin.registerButtonLabel"));
+		},
 
-	
-function addUserLoginEventListeners() {
-	$("#user_register_button").on('click', function() {
-		$('#userLoginForm').jqxValidator('hide');
-	});
+		initUserLoginElements:function () {
+			
+		    $("#userLoginDiv").jqxExpander({ toggleMode: 'none', width: '300px', showArrow: false, theme: theme });
+		    $('#user_login_button').jqxButton({ width: 60, height: 25, theme: theme });
+		    $('#user_register_button').jqxButton({ width: 60, height: 25, theme: theme });
+		    
+			$('#user_account').jqxInput({width: 120, height: 20, theme: theme });
+			$('#user_password').jqxInput({width: 120, height: 20, theme: theme });
+		    
+		    $('#userLoginForm').jqxValidator({
+		     rules: [
+		            { input: '#user_account', message: i18n.t("userLogin.validation.message.requireAccount"), action: 'keyup, blur', rule: 'required' },
+		            { input: '#user_account', message: i18n.t("userLogin.validation.message.accountLength"), action: 'keyup, blur', rule: 'length=3,15' },
+		            { input: '#user_password', message: i18n.t("userLogin.validation.message.requirePassword"), action: 'keyup, blur', rule: 'required' },
+		            { input: '#user_password', message: i18n.t("userLogin.validation.message.passwordLength"), action: 'keyup, blur', rule: 'length=6,20' }
+		            ], 
+		            theme: theme
+		    });
+			
+		},
+			
+		addUserLoginEventListeners:function () {
+			$("#user_register_button").bind('click', function(event) {
+				$('#userLoginForm').jqxValidator('hide');
+			});
 
-	$("#user_login_button").on('click', function() {
-		$('#userLoginForm').jqxValidator('validate');
-	});
+			$("#user_login_button").bind('click', function() {
+				$('#userLoginForm').jqxValidator('validate');
+			});
 
-	$('#userLoginForm').on('validationSuccess', function(event) {
-		var userAccount = $('#user_account').val();
-		var userPassword = $('#user_password').val();
-		if (userAccount != null && userPassword != null) {
-			var loginUser = {};
-			loginUser["user.account"] = userAccount;
-			loginUser["user.password"] = userPassword;
-			$.post("loginUser.action", loginUser, function(result) {
-		    	
-				if(result != null && result["id"] != null) {
-					window.location.href="/main.html";
-				} else {
-					$('#user_password').val(null);
-					$("#eventLog").text("注册失败");
+			$('#userLoginForm').bind('validationSuccess', function(event) {
+				var userAccount = $('#user_account').val();
+				var userPassword = $('#user_password').val();
+				if (userAccount != null && userPassword != null) {
+					var loginUser = {};
+					loginUser["user.account"] = userAccount;
+					loginUser["user.password"] = userPassword;
+					AppUtil.request("loginUser.action", loginUser, function(result){
+				    	
+						if(result != null && result["id"] != null) {
+							window.location.href="main.html";
+						} else {
+							$('#user_password').val(null);
+							$("#eventLog").text("注册失败");
+						}
+					},function(){
+						alert("Fail to login!");
+					});
+					
 				}
 			});
-			
-		}
-	});
 
-}
+		}
+};
+
 	
 	
