1117153e594aa932037755d4162915ef6e4b57b5
hzeng135 <hzeng135@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-09-07 04:57:45 +0000

    fixed most i18n issue and window position and size issue.

--- trunk/WebSecurityServer/WebContent/js/pitaya/systemUtil.js
+++ trunk/WebSecurityServer/WebContent/js/pitaya/systemUtil.js
@@ -1,3 +1,24 @@
+function contains(string,substr,isIgnoreCase)
+{
+    if(isIgnoreCase)
+    {
+     string=string.toLowerCase();
+     substr=substr.toLowerCase();
+    }
+     var startChar=substr.substring(0,1);
+     var strLen=substr.length;
+         for(var j=0;j<string.length-strLen+1;j++)
+         {
+             if(string.charAt(j)==startChar)
+             {
+                   if(string.substring(j,j+strLen)==substr)
+                   {
+                         return true;
+                   }   
+             }
+         }
+         return false;
+}
 
 function callAction(actionName,parameter) {
 		var result = {};
