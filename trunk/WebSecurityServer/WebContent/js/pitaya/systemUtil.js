2e455c77fc20d73f668667832c3d72902bac0067
hzeng135 <hzeng135@658d62ce-3a00-bcee-73d7-f3445d85faba>
2013-09-13 07:31:22 +0000

    add highlight feature in table grid

--- trunk/WebSecurityServer/WebContent/js/pitaya/systemUtil.js
+++ trunk/WebSecurityServer/WebContent/js/pitaya/systemUtil.js
@@ -137,4 +137,15 @@ Date.prototype.Format = function (fmt) {
     return fmt;
 };
 
-
+Array.prototype.remove=function(dx) 
+{ 
+    if(isNaN(dx)||dx>this.length){return false;} 
+    for(var i=0,n=0;i<this.length;i++) 
+    { 
+        if(this[i]!=this[dx]) 
+        { 
+            this[n++]=this[i]; 
+        } 
+    } 
+    this.length-=1;
+} 
